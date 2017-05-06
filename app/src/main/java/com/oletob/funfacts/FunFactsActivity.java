7e3cfacdad5f73f14b37252ef31e573dae88546f
Algenis E. Volquez <evolquez@gmail.com>
2017-05-06 19:49:02 -0400

    Accesing views in code: Declaring and assigning variables

--- app/src/main/java/com/oletob/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/oletob/funfacts/FunFactsActivity.java
@@ -2,12 +2,21 @@ package com.oletob.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
+    //Declare our View variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Assings the Views from the layout file to the corresponding variables
+        factTextView = (TextView) findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFactButton);
     }
 }
