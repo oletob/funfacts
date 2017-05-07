b275c61855ae1fcee1d2dc43f2ec01b1d94008e1
Algenis E. Volquez <evolquez@gmail.com>
2017-05-06 22:25:05 -0400

    Refactoring, making a new class to handler facts

---
+++ app/src/main/java/com/oletob/funfacts/FactBook.java
@@ -0,0 +1,30 @@
+package com.oletob.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by evolquez on 5/6/17.
+ */
+
+public class FactBook {
+
+    // Fields (Member variables)
+    private String[] facts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+    // Methods
+    public String getFact(){
+        
+        // Randomly select a fact
+        return facts[(new Random().nextInt(facts.length))];
+    }
+}
--- app/src/main/java/com/oletob/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/oletob/funfacts/FunFactsActivity.java
@@ -9,20 +9,11 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+    private FactBook factBook = new FactBook();
+
     // Declare our View variables
     private TextView factTextView;
     private Button showFactButton;
-    public static String[] facts = {
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built." };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +30,8 @@ public class FunFactsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // The button was clicked, so update the fact TextView with a new fact
 
-                // Randomly select a fact
-                int randomNumber = new Random().nextInt(FunFactsActivity.facts.length);
-
                 // Update the screen with out dynamic fact
-                factTextView.setText(FunFactsActivity.facts[randomNumber]);
+                factTextView.setText(factBook.getFact());
             }
         };
 
