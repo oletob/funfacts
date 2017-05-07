8a3ce69298546d075b802542bec67c5e4caa87df
Algenis E. Volquez <evolquez@gmail.com>
2017-05-06 20:44:22 -0400

    Makes fun facts a bit dynamically by adding some facts. Adding the onclick listener method.

--- app/src/main/java/com/oletob/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/oletob/funfacts/FunFactsActivity.java
@@ -2,21 +2,51 @@ package com.oletob.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class FunFactsActivity extends AppCompatActivity {
-    //Declare our View variables
+    // Declare our View variables
     private TextView factTextView;
     private Button showFactButton;
+    public static String[] facts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built." };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
-        //Assings the Views from the layout file to the corresponding variables
+        // Assings the Views from the layout file to the corresponding variables
         factTextView = (TextView) findViewById(R.id.factTextView);
         showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        // Declares a listener for button
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The button was clicked, so update the fact TextView with a new fact
+
+                // Randomly select a fact
+                int randomNumber = new Random().nextInt(FunFactsActivity.facts.length);
+
+                // Update the screen with out dynamic fact
+                factTextView.setText(FunFactsActivity.facts[randomNumber]);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
     }
 }
